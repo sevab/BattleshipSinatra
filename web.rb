622e17fae89a0e65f0b2af14078a62967c25582a
Seva <vabaskin@gmail.com>
2013-07-11 15:33:29 +0100

    migrated to rackup; running everything from the class BattleshipWeb; yet to implement mu;tiplayer

--- web.rb
+++ web.rb
@@ -1,44 +1,68 @@
 require './ship'
 require './grid_refactored'
-require 'sinatra'
-
-@@grid = Grid.new
-#@@fleet = {carrier: 5, battleship: 4, cruiser: 3, destroyer: 2, submarine: 1}
-@@fleet = [[:carrier, 5], [:battleship, 4], [:cruiser, 3], [:destroyer, 2], [:submarine, 1]]
-@@settings = {}
-get '/start' do
-	@a = :A
-	@b = 1
-	@start = "A1"
-	erb :start
-end
+require 'sinatra/base'
 
 
-post '/start' do
-	while !@@fleet.empty?
-	  @@grid.place(
-	    Ship.new(@@fleet.shift[1]),
-		params['orientation'].to_sym,
-		params['coordinate'].to_sym)
-		
-		redirect '/game' if @@fleet.empty?
-		redirect '/start'
+class BattleWeb < Sinatra::Base
+	enable :sessions
+
+	@@grid = Grid.new
+	#@@fleet = {carrier: 5, battleship: 4, cruiser: 3, destroyer: 2, submarine: 1}
+	@@fleet = [[:carrier, 5], [:battleship, 4], [:cruiser, 3], [:destroyer, 2], [:submarine, 1]]
+	@@settings = {}
+
+	get '/test/who' do
+		session[:who]
+	end
+
+	get '/test' do
+		session[:who]=rand.to_s
+	end
+
+
+
+	get '/start' do
+		#session[:me]= 'cool cat'
+		@a = :A
+		@b = 1
+		@start = "A1"
+		@grid = @@grid
+		@fleet = @@fleet
+		erb :start
+	end
+
+
+	post '/start' do
+		#puts session[:me]
+		@grid = @@grid
+		@fleet = @@fleet
+		while !@fleet.empty?
+		  @grid.place(
+		    Ship.new(@fleet.shift[1]),
+			params['orientation'].to_sym,
+			params['coordinate'].to_sym)
+			
+			redirect '/game' if @fleet.empty?
+			redirect '/start'
+		end
+	end
+
+	get '/game' do
+		@a = :A
+		@b = 1
+		@start = "A1"
+		@grid = @@grid
+		erb :game
+	end
+
+	post '/game' do
+		@grid = @@grid
+		@grid.hit?(params['coordinate'].to_sym)
+		redirect '/game'
 	end
 end
-
-get '/game' do
-	@a = :A
-	@b = 1
-	@start = "A1"
-	erb :game
-end
-
-post '/game' do
-	@@grid.hit?(params['coordinate'].to_sym)
-	redirect '/game'
-end
-
-=begin
+__END__
 # => Create a player
-# => raise exceptions
-=end
+# => raise exceptions:
+# => => when go back from /game to /start, return an error, or redirect back to /game
+#on the game view display your own ships & update sunk cells
